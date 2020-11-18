ff312ef9507e4b3229c36cde04c1fbee85933e94
Faisal <faisal.j@ufaber.com>
2020-11-18 17:25:28 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,477 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppRoot());
+  runApp(Menu());
 }
 
-class AppRoot extends StatelessWidget {
+class MenuItem extends StatelessWidget {
+  const MenuItem(this.icon, this.itemText);
+  final String icon;
+  final String itemText;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(
+        icon,
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+      title: Text(itemText),
+    );
+  }
+}
 
-  static final showGrid = false; // Set to false to show ListView
+class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-              "Flutter Learning"
+          title: Text('Menu Demo'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              // Modify code here
+              Example1(),
+              Example2(),
+              Example3(),
+            ],
           ),
         ),
-        body:  Center(child: showGrid ? _buildGrid() : _buildList()),
+      ),
+    );
+  }
+}
+
+// Problem 1: Overflow error
+class Example1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              'Explore the restaurant\'s delicious menu items below!',
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Problem 2: Viewport was given unbounded height error
+class Example2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: [
+          MenuItem('🍔', 'Burger'),
+          MenuItem('🌭', 'Hot Dog'),
+          MenuItem('🍟', 'Fries'),
+          MenuItem('🥤', 'Soda'),
+          MenuItem('🍦', 'Ice Cream'),
+        ],
+      ),
+    );
+  }
+}
+
+// Problem 3: Invisible VerticalDivider
+class Example3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RaisedButton(
+            onPressed: () {
+              print('Pickup button pressed.');
+            },
+            child: Text(
+              'Pickup',
+            ),
+          ),
+          // This widget is not shown on screen initially.
+          VerticalDivider(
+            width: 20.0,
+            thickness: 5.0,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('Delivery button pressed.');
+            },
+            child: Text(
+              'Delivery',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Nav2App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Navigating screens",
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/details' : (context) => DetailPage()
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nav2App"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("View Details"),
+          onPressed: () {
+            Navigator.pushNamed(context, '/details');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FlatButton(
+          child: Text('Pop!'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
     );
   }
 }
 
 
-class ScrollViewApp extends StatelessWidget {
+
+class HeroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: MyApp(),
+    return MaterialApp(
+      title: 'Transition Demo',
+      home: MainScreen(),
     );
   }
 }
 
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://picsum.photos/250?image=9',
+          ),
+        ),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return DetailScreen();
+          }));
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
+// TapboxA manages its own state.
+
+//------------------------- TapboxA ----------------------------------
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+//------------------------- MyApp ----------------------------------
+
+class MyApp3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+        ),
+        body: Center(
+          child: TapboxA(),
+        ),
+      ),
+    );
+  }
+}
+
+class FavouriteWIdget extends StatefulWidget {
+  @override
+  _FavouriteWIdgetState createState() => _FavouriteWIdgetState();
+}
+
+class _FavouriteWIdgetState extends State<FavouriteWIdget> {
+  bool _isFavourite = true;
+  int _favourite = 41;
+
+  void _toggleFavourite() {
+    setState(() {
+      if (_isFavourite) {
+        _favourite = 40;
+        _isFavourite = !_isFavourite;
+      }
+      else {
+        _favourite = 41;
+        _isFavourite = !_isFavourite;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: _isFavourite ? Icon(Icons.star) : Icon(Icons.star_border),
+            color: Colors.red[500],
+            onPressed: _toggleFavourite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favourite'),
+          ),
+        )
+      ],
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          FavouriteWIdget(),
+        ],
+      ),
+    );
+
+    Color color = Theme
+        .of(context)
+        .primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.asset(
+                'assets/1.jpg',
+              ),
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AppRoot extends StatelessWidget {
+  static final showGrid = true; // Set to false to show ListView
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Learning"),
+        ),
+        body: ScrollViewApp(),
+      ),
+    );
+  }
+}
+
+class ScrollViewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: _buildCard(),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -44,81 +481,143 @@ class MyApp extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         textDirection: TextDirection.rtl,
         children: [
-          Expanded(child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 10, color: Colors.blue[800]),
-                  borderRadius: const BorderRadius.all(
-                      const Radius.circular(8)),
-                ),
-                child: Image(
-                    image: NetworkImage(
-                        'https://c4.wallpaperflare.com/wallpaper/186/195/695/underwater-world-herd-blue-yellow-medium-size-fish-wallpaper-preview.jpg')
-                ),
-              ),
-            ],
-          ))
+          Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 10, color: Colors.blue[800]),
+                      borderRadius:
+                      const BorderRadius.all(const Radius.circular(8)),
+                    ),
+                    child: Image(
+                        image: NetworkImage(
+                            'https://c4.wallpaperflare.com/wallpaper/186/195/695/underwater-world-herd-blue-yellow-medium-size-fish-wallpaper-preview.jpg')),
+                  ),
+                ],
+              ))
         ],
       ),
     );
   }
 }
 
+Widget _buildCard() =>
+    AspectRatio(
+      aspectRatio: 16 / 9,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('1625 Main Street',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('(408) 555-1212',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
 class GridList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _buildGrid();
+    return _buildStack();
   }
 }
 
-Widget _buildGrid() => GridView.extent(
-    maxCrossAxisExtent: 150,
-    padding: const EdgeInsets.all(4),
-    mainAxisSpacing: 4,
-    crossAxisSpacing: 4,
-    children: _buildGridTileList(30));
+Widget _buildStack() =>
+    Stack(
+      alignment: const Alignment(0.6, 0.6),
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage('assets/2.jpg'),
+          radius: 100,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black45,
+          ),
+          child: Text(
+            'Mia B',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+
+Widget _buildGrid() =>
+    GridView.extent(
+        maxCrossAxisExtent: 150,
+        padding: const EdgeInsets.all(4),
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        children: _buildGridTileList(30));
 
 // The images are saved with names pic0.jpg, pic1.jpg...pic29.jpg.
 // The List.generate() constructor allows an easy way to create
 // a list when objects have a predictable naming pattern.
-List<Container> _buildGridTileList(int count) => List.generate(
-    count, (i) => Container(child: Image.asset('assets/1.jpg')));
+List<Container> _buildGridTileList(int count) =>
+    List.generate(count, (i) => Container(child: Image.asset('assets/1.jpg')));
 // #enddocregion grid
 
 // #docregion list
-Widget _buildList() => ListView(
-  children: [
-    _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
-    _tile('The Castro Theater', '429 Castro St', Icons.theaters),
-    _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
-    _tile('Roxie Theater', '3117 16th St', Icons.theaters),
-    _tile('United Artists Stonestown Twin', '501 Buckingham Way',
-        Icons.theaters),
-    _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-    Divider(),
-    _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-    _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
-    _tile(
-        'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
-    _tile('La Ciccia', '291 30th St', Icons.restaurant),
-  ],
-);
+Widget _buildList() =>
+    ListView(
+      children: [
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+            Icons.theaters),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+        _tile('La Ciccia', '291 30th St', Icons.restaurant),
+      ],
+    );
 
-ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
-  title: Text(title,
-      style: TextStyle(
-        fontWeight: FontWeight.w500,
-        fontSize: 20,
-      )),
-  subtitle: Text(subtitle),
-  leading: Icon(
-    icon,
-    color: Colors.blue[500],
-  ),
-);
+ListTile _tile(String title, String subtitle, IconData icon) =>
+    ListTile(
+      title: Text(title,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          )),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
+      ),
+    );
 // #enddocregion list
 
 class TextWid extends StatelessWidget {
@@ -128,10 +627,7 @@ class TextWid extends StatelessWidget {
       "subtitle",
       textDirection: TextDirection.rtl,
       style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          color: Colors.orange
-      ),
+          fontSize: 20, fontWeight: FontWeight.w600, color: Colors.orange),
     );
   }
 }
@@ -151,8 +647,7 @@ class application extends StatelessWidget {
                 color: Colors.blue,
                 fontFamily: 'Raleway',
                 fontSize: 50,
-                fontWeight: FontWeight.w600
-            ),
+                fontWeight: FontWeight.w600),
           ),
         ),
       ),
